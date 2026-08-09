7e2222a39f9c559ed56187adfa70738be4fa018d
Achyuth Yandamuri <60690096+achyuto2@users.noreply.github.com>
2026-08-09 16:56:08 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "mustafa.devops77" # Replace with your actual S3 bucket name
     key    = "Prod/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
